3fcd8278b858c257c1fa0bdd91089987a56f6064
Reuber99 <115495199+Reuber99@users.noreply.github.com>
2022-10-10 20:11:37 +0200

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/6AcKGg8j42")
-game:GetService("Players").LocalPlayer:Kick("User has been banned for exploiting. Appeal server is copied to clipboard. Time left:99D 59H 59M 59S")
+game:GetService("Players").LocalPlayer:Kick("User has been banned for exploiting. Appeal server is copied to clipboard. Time left : 99 Days 59 Hours 59 Minutes 59 Seconds")
